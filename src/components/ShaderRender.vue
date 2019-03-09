819d75d766009dd324b7f726361d78773dc819c4
jules goullée <julesgoullee@gmail.com>
2019-03-09 15:20:11 +0100

    shaderRender component

---
+++ src/components/ShaderRender.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    id="shader-container"
+    class="home"
+  />
+</template>
+
+<script>
+  import ShaderEngine from '@/modules/ShaderEngine'
+  import shader1 from '@/shaders/shader1'
+
+
+  export default {
+    name: 'ShaderRender',
+    data: () => ({
+      shaderEngine: null,
+    }),
+    mounted: async function() {
+
+      const container = document.getElementById( 'shader-container' );
+
+      this.shaderEngine = new ShaderEngine(shader1, container);
+      this.shaderEngine.init();
+      this.shaderEngine.start();
+
+    },
+    beforeDestroy: async function() {
+
+      this.shaderEngine.stop();
+
+    }
+  }
+</script>
--- src/views/Shader.vue
+++ src/views/Shader.vue
@@ -1,30 +1,17 @@
 <template>
-  <div class="home" id="shader-container" />
+  <div class="home">
+    <ShaderRender />
+  </div>
 </template>
 
 <script>
-  import ShaderEngine from '@/modules/ShaderEngine'
-  import shader1 from '@/shaders/shader1'
 
+  import ShaderRender from '@/components/ShaderRender.vue'
 
   export default {
-    name: 'Home',
-    data: () => ({
-      shaderEngine: null,
-    }),
-    mounted: async function() {
-
-      const container = document.getElementById( 'shader-container' );
-
-      this.shaderEngine = new ShaderEngine(shader1, container);
-      this.shaderEngine.init();
-      this.shaderEngine.start();
-
-    },
-    beforeDestroy: async function() {
-
-      this.shaderEngine.stop();
-
-    },
+    name: 'Shader',
+    components: {
+      ShaderRender
+    }
   }
 </script>
