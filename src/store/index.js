e690418e9bd8aab1987872db3fa089dea5d6d2f8
jules goullée <julesgoullee@gmail.com>
2019-03-10 05:12:44 +0100

    style

--- src/store/index.js
+++ src/store/index.js
@@ -102,7 +102,7 @@ export const store = {
 
       if(state.shaderEngine){
 
-        commit('setMidiValue', { controlId, value} );
+        commit('setMidiValue', { controlId, value });
 
       }
 
