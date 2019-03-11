ef9acfae003c692a487c3f1d3efe5cdb501e5409
Pacien Boisson <pakokrew@gmail.com>
2019-03-11 03:33:04 +0100

    new shader

--- src/shaders/index.js
+++ src/shaders/index.js
@@ -1,11 +1,13 @@
 import shader1 from '@/shaders/shader1';
 import shader2 from '@/shaders/shader2';
 import shader3 from '@/shaders/shader3';
+import shader4 from '@/shaders/shader4';
 
 const shaders = {
   shader1,
   shader2,
   shader3,
+  shader4,
 };
 
 export default shaders;
