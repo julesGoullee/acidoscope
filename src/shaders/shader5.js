20dace86b2077ae820ac9816d395efb43ba9bd33
jules goullée <julesgoullee@gmail.com>
2019-03-16 18:39:14 +0100

    new shaders & uniforms injection (#23)
    * new shaders & uniforms injection
    
    * screenshots

---
+++ src/shaders/shader5.js
@@ -0,0 +1,8 @@
+
+export default {
+  original: 'https://www.shadertoy.com/view/3dlSz2',
+  name: 'Grid Landscape',
+  wrapper: 'image',
+  fragmentShader: require('./shader5.glsl'),
+  //params: [],
+}
