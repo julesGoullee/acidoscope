8a33c8e9f369f310ab3c9fea515b3764dcb861c0
jules goullée <julesgoullee@gmail.com>
2019-03-17 01:56:45 +0100

    phase params inside shaders

--- src/store/link.js
+++ src/store/link.js
@@ -6,6 +6,7 @@ const LinkModule = {
 
   state: {
     linkConnected: false,
+    linkEnabled: false
   },
 
   mutations: {
@@ -13,24 +14,22 @@ const LinkModule = {
     setLinkStatus(state, linkConnected) {
       state.linkConnected = linkConnected;
     },
-
+    setLinkEnable(state, linkEnabled) {
+      state.linkEnabled = linkEnabled;
+    },
   },
   actions: {
 
-    initLink({ commit, rootState }){
+    initLink({ commit, dispatch }){
 
       link.init();
 
+      commit('setLinkEnable', true);
       link.on('statusChanged', (linkConnected) => {
 
         if(!linkConnected){
 
-          rootState.shader.shaderEngine.shaderParams.setBeat({
-            beatStartTime: 0,
-            bps: 1,
-            bpm: 60,
-            beat: 0,
-          });
+          dispatch('switchLinkEnable', false);
 
         }
 
@@ -40,13 +39,40 @@ const LinkModule = {
 
     },
 
-    listenLinkActions({ rootState } ){
+    listenLinkActions({ rootState, state } ){
 
       link.on('beat', (beatData) => {
 
-        rootState.shader.shaderEngine.shaderParams.setBeat(beatData);
+        if(state.linkEnabled){
+
+          rootState.shader.shaderEngine.shaderParams.setBeat(beatData);
+
+        }
 
       });
+
+    },
+
+    switchLinkEnable({ rootState, commit }, enable){
+
+      if(enable){
+
+        return commit('setLinkEnable', true);
+
+      }
+
+      if(rootState.shader.shaderEngine){
+
+        rootState.shader.shaderEngine.shaderParams.setBeat({
+          beatStartTime: 0,
+          bps: 1,
+          bpm: 60,
+          beat: 0,
+        });
+
+      }
+
+      commit('setLinkEnable', false);
 
     },
 
@@ -60,6 +86,7 @@ const LinkModule = {
 
   getters: {
     linkConnected: state => state.linkConnected,
+    linkEnabled: state => state.linkEnabled,
   },
 
 };
