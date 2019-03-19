2d5b40eab1a2eebc49b37c1f27b24f14648e3c19
Pacien Boisson <pakokrew@users.noreply.github.com>
2019-03-19 22:41:00 +0100

    Switchviz (#22)
    * set visualisation
    
    * next visualisation
    
    * change touch next

--- src/views/Shader.vue
+++ src/views/Shader.vue
@@ -1,5 +1,6 @@
 <template>
   <v-container
+    v-if="selectedVisualization"
     class="shader-view"
   >
     <v-flex
@@ -27,9 +28,12 @@
 
 <script>
 
+  import assert from 'assert';
+
   import ShaderControl from '@/components/ShaderControl.vue'
   import ShaderRender from '@/components/ShaderRender.vue'
   import ShaderParams from '@/components/ShaderParams.vue';
+  import { mapState, mapActions, mapGetters } from 'vuex';
 
   export default {
     name: 'ShaderView',
@@ -38,5 +42,29 @@
       ShaderRender,
       ShaderControl,
     },
+    computed: {
+      ...mapGetters([
+        'selectedVisualization',
+      ]),
+      ...mapState([
+        'route',
+      ]),
+    },
+    mounted() {
+
+      assert(this.route.params && this.route.params.id, 'invalid_route');
+
+      const shaderId = this.route.params.id;
+
+      this.loadVisualisations();
+      this.setVisualisation(shaderId);
+
+    },
+    methods: {
+      ...mapActions([
+        'loadVisualisations',
+        'setVisualisation',
+      ]),
+    }
   }
 </script>
